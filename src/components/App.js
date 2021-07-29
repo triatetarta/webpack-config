afd77ca2e64f9116632bc641d93dc8c8776122d7
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2021-07-29 18:51:49 +0100

    framer motion scroll practice

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,50 @@
-import '../styles/index.scss';
-import Recipes from './Recipes';
-import sword from '../images/swc-sword.png';
-import swordSvg from '../images/sword.svg';
+import { useTransform, useViewportScroll, motion } from 'framer-motion';
+import { useEffect, useState } from 'react';
 
 const App = () => {
-  return (
-    <>
-      <section className='hero'></section>
-      <main>
-        <section>
-          <h1>Oh Hey, React man</h1>
-        </section>
-      </main>
+  const [trigger, setTrigger] = useState(false);
+  const { scrollYProgress, scrollY } = useViewportScroll();
+  const scaleAnim = useTransform(scrollYProgress, [0, 0.1, 1], [1, 1.2, 1.8]);
+  const yPosAnim = useTransform(scrollYProgress, [0, 0.1, 1], [0, 0, 0]);
 
-      <img src={sword} alt='sword' width='250' />
-      <img src={swordSvg} alt='sword' width='250' />
-      <Recipes />
-    </>
+  useEffect(() => {
+    scrollY.onChange((x) => {
+      if (x >= 200) {
+        setTrigger(true);
+      }
+      if (x <= 200) {
+        setTrigger(false);
+      }
+    });
+
+    return () => scrollY.destroy();
+  }, [scrollY]);
+
+  return (
+    <div className='h-[200vh]'>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+        style={{ scale: scaleAnim, y: yPosAnim }}
+        className='fixed top-[20rem] left-[calc(50%-14rem)] w-[28rem] h-[15rem] bg-red-500 scale-100 overflow-hidden'
+      >
+        <motion.div
+          initial={{ opacity: 0, scale: 0, y: '-100%' }}
+          animate={{
+            opacity: trigger ? 1 : 0,
+            scale: trigger ? 1 : 0,
+            y: trigger ? '0%' : '-100%',
+          }}
+          transition={{ type: 'spring', stiffness: 250, damping: 25 }}
+          className='w-full h-full flex items-center justify-center p-4 text-white'
+        >
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore
+          aspernatur asperiores minima impedit minus est illum quam excepturi
+          debitis quo.
+        </motion.div>
+      </motion.div>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './styles/main.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
